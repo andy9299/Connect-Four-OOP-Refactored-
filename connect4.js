55ac530aeb29dd905c903387d3b67a7607c55f09
andy9299 <andy.tu998@gmail.com>
2022-07-26 16:39:57 -0400

    init

---
+++ connect4.js
@@ -0,0 +1,164 @@
+/** Connect Four
+ *
+ * Player 1 and 2 alternate turns. On each turn, a piece is dropped down a
+ * column until a player gets four-in-a-row (horiz, vert, or diag) or until
+ * board fills (tie)
+ */
+
+const WIDTH = 7;
+const HEIGHT = 6;
+
+let currPlayer = 1; // active player: 1 or 2
+let board = []; // array of rows, each row is array of cells  (board[y][x])
+
+/** makeBoard: create in-JS board structure:
+ *    board = array of rows, each row is array of cells  (board[y][x])
+ */
+
+function makeBoard() {
+  for (let i = 0; i < HEIGHT; i++) {
+    board.push(Array.from({ length: WIDTH }));
+  }
+}
+
+/** makeHtmlBoard: make HTML table and row of column tops. */
+
+function makeHtmlBoard() {
+  const htmlBoard = document.querySelector('#board');
+
+  // creating and labeling the top row of cells
+  // where a user can click to trigger the handleClick fn
+  const top = document.createElement("tr");
+  top.setAttribute("id", "column-top");
+  top.addEventListener("click", handleClick);
+
+  for (let x = 0; x < WIDTH; x++) {
+    const headCell = document.createElement("td");
+    headCell.setAttribute("id", x);
+    top.append(headCell);
+  }
+  htmlBoard.append(top);
+
+  // creating and label each row of cells by looping
+  // over and adding the y-axis (<tr>)
+  for (let y = 0; y < HEIGHT; y++) {
+    const row = document.createElement("tr");
+    for (let x = 0; x < WIDTH; x++) {
+      const cell = document.createElement("td");
+      cell.setAttribute("id", `${y}-${x}`);
+      row.append(cell);
+    }
+    htmlBoard.append(row);
+  }
+}
+
+/** findSpotForCol: given column x, return top empty y (null if filled) */
+function findSpotForCol(x) {
+  for (let y = HEIGHT - 1; y >= 0; y--) {
+    if (!board[y][x]) {
+      return y;
+    }
+  }
+}
+
+/** placeInTable: update DOM to place piece into HTML table of board */
+
+function placeInTable(y, x) {
+  const piece = document.createElement("div");
+  piece.classList.add("piece");
+  (currPlayer === 1) ? piece.classList.add("p1") : piece.classList.add("p2");
+  const placement = document.getElementById(`${y}-${x}`);
+  placement.append(piece);
+}
+
+/** endGame: announce game end and create a reset button*/
+
+function endGame(msg) {
+  alert(msg);
+  const resetBtn = document.createElement("button");
+  resetBtn.innerHTML = "Reset Game!";
+  resetBtn.setAttribute("id", "resetBtn");
+  resetBtn.addEventListener("click", resetGame);
+  const game = document.querySelector('#game');
+  game.appendChild(resetBtn);
+
+}
+
+/** handleClick: handle click of column top to play piece */
+
+function handleClick(evt) {
+  // get x from ID of clicked cell
+  const x = +evt.target.id;
+
+  // get next spot in column (if none, ignore click)
+  const y = findSpotForCol(x);
+  if (y === null) {
+    return;
+  }
+
+  // place piece in board and add to HTML table
+  placeInTable(y, x);
+  board[y][x] = currPlayer;
+
+  // check for win
+  if (checkForWin()) {
+    return endGame(`Player ${currPlayer} won!`);
+  }
+
+  // check for tie
+  if (board.every(row => row.every(cell => cell))) { return endGame("Tie Game!"); }
+
+
+  // switch players
+  currPlayer = currPlayer === 1 ? 2 : 1;
+}
+
+/** checkForWin: check board cell-by-cell for "does a win start here?" */
+
+function checkForWin() {
+  function _win(cells) {
+    // Check four cells to see if they're all color of current player
+    //  - cells: list of four (y, x) cells
+    //  - returns true if all are legal coordinates & all match currPlayer
+
+    return cells.every(
+      ([y, x]) =>
+        y >= 0 &&
+        y < HEIGHT &&
+        x >= 0 &&
+        x < WIDTH &&
+        board[y][x] === currPlayer
+    );
+  }
+
+  // for every cell check if theres a winner going horizontal, vertical,
+  // or both diagonals. it will cover everything since it goes through every cell
+
+  for (var y = 0; y < HEIGHT; y++) {
+    for (var x = 0; x < WIDTH; x++) {
+      const horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
+      const vert = [[y, x], [y + 1, x], [y + 2, x], [y + 3, x]];
+      const diagDR = [[y, x], [y + 1, x + 1], [y + 2, x + 2], [y + 3, x + 3]];
+      const diagDL = [[y, x], [y + 1, x - 1], [y + 2, x - 2], [y + 3, x - 3]];
+
+      if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
+        return true;
+      }
+    }
+  }
+}
+
+// resets the game by remaking the board/htmlboard and removing the reset button
+
+function resetGame() {
+  board = [];
+  const htmlBoard = document.querySelector('#board');
+  htmlBoard.innerHTML = "";
+  const resetBtn = document.querySelector('#resetBtn');
+  resetBtn.remove();
+  makeBoard();
+  makeHtmlBoard();
+}
+
+makeBoard();
+makeHtmlBoard();
